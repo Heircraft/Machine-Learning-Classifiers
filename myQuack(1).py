158838fe5134e40b9a82ea7e42d11329115c86d2
Heircraft <alexanderflsantander@gmail.com>
2018-06-03 22:33:44 +1000

    ready for submission

--- myQuack(1).py
+++ myQuack(1).py
@@ -52,9 +52,7 @@ def prepare_dataset(dataset_path):
     @return
 	X,y
     '''
-    ##         "INSERT YOUR CODE HERE"   
-    # validation in classifier
-
+    #open the file and read in all the data
     fl = open(dataset_path, "r");
     
     line = fl.readline();
@@ -70,6 +68,7 @@ def prepare_dataset(dataset_path):
     y1 = [];
     X1 = [];
     n = rowsArr.shape[0];
+    #iterate through the file and assign all the data
     for x in range(n):
         X1.append(rowsArr[x][2:]);
         Y1 = rowsArr[x][1];
@@ -80,7 +79,8 @@ def prepare_dataset(dataset_path):
      
     y = np.array(y1);
     X = np.array(X1);
-
+    
+    #split the data 90% training and 10% testing
     n9 = int(n*0.9);
     Xtrain, Xtest = X[:n9], X[n9:];
     ytrain, ytest = y[:n9], y[n9:];
@@ -101,10 +101,11 @@ def build_NB_classifier(X_training, y_training):
     @return
 	clf : the classifier built in this function
     '''
-     
+    #fit the data to the GaussianNB() classifier
     X_training = X_training.astype(np.float);
     clf = GaussianNB();
     clf.fit(X_training, y_training);    
+    #return the mean of 10 cross validated scores
     scores = cross_val_score(clf, X_training, y_training, cv=10, scoring='accuracy').mean();
     print("Cross validated accuracy: ", scores) 
     
@@ -123,12 +124,13 @@ def build_DT_classifier(X_training, y_training):
     @return
 	clf : the classifier built in this function
     '''
-    
+    #the range of values used to find the best value for our hyperparameter
     d_range = range(1, 20);
     d_scores = [];
     
-    
+    #iterate over range and cross validate 10 times each iteration
     for d in d_range:
+        #set random state to ensure a constant randomness
         clf = DecisionTreeClassifier(max_depth =d, random_state=20);
         scores = cross_val_score(clf, X_training, y_training, cv=10, scoring='accuracy').mean();
         
@@ -137,7 +139,7 @@ def build_DT_classifier(X_training, y_training):
         
         d_scores.append(round(scores.mean(), 5));
     
-
+    #plot
     plt.scatter(d_range, d_scores)
     z = np.polyfit(d_range, d_scores, 4)
     p = np.poly1d(z)
@@ -149,10 +151,11 @@ def build_DT_classifier(X_training, y_training):
     # best_d (depth) is the hyperparameter
     print("Best D value = ", best_d);
     
+    #take our best parameter and input it in our classifier
     dtf = DecisionTreeClassifier(max_depth = best_d, random_state = 10);
-    
     dtf.fit(X_training, y_training)
     
+    #return the mean of 10 cross validated scores to find cross validated accuracy
     score = cross_val_score(dtf, X_training, y_training, cv=10, scoring='accuracy').mean();
     
     print("Cross-validated score with best d: ", score);
@@ -175,10 +178,11 @@ def build_NN_classifier(X_training, y_training):
 	clf : the classifier built in this function
     ''' 
     
-#    after range of 50 the validation accuracy almost never rises on average
+    #the range of values used to find the best value for our hyperparameter
     k_range = range(1, 51);
     k_scores = [];
     
+    #iterate over range and cross validate 10 times each iteration
     for k in k_range:
         clf = KNeighborsClassifier(n_neighbors=k);
         scores = cross_val_score(clf, X_training, y_training, cv=10, scoring='accuracy');
@@ -188,6 +192,7 @@ def build_NN_classifier(X_training, y_training):
         
         k_scores.append(round(scores.mean(), 5));
     
+    #plot
     plt.scatter(k_range, k_scores)
     z = np.polyfit(k_range, k_scores, 3)
     p = np.poly1d(z)
@@ -199,10 +204,11 @@ def build_NN_classifier(X_training, y_training):
     # best_k is the hyperparameter
     print("Best K value = ", best_k);
     
+    #take our best parameter and input it in our classifier
     knn = KNeighborsClassifier(n_neighbors=best_k);
-    
     knn.fit(X_training, y_training)
     
+    #return the mean of 10 cross validated scores to find cross validated accuracy
     score = cross_val_score(knn, X_training, y_training, cv=10, scoring='accuracy').mean();
     
     print("Cross-validated score with best K: ", score);
@@ -211,8 +217,6 @@ def build_NN_classifier(X_training, y_training):
     
     return knn;
     
-#STANDARD DEVIATION DO SOMETHING HERE
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 def build_SVM_classifier(X_training, y_training):
@@ -226,12 +230,12 @@ def build_SVM_classifier(X_training, y_training):
     @return
 	clf : the classifier built in this function
     '''
-    ##         "INSERT YOUR CODE HERE"    
-    # linearsvc is same as kernel=linear Go with JUST SVC 
-    
+
+    #the range of values used to find the best value for our hyperparameter
     g_range = np.logspace(-6, -1, 5);
     g_scores = [];
     
+    #iterate over range and cross validate 10 times each iteration
     for g in g_range:
         clf = SVC(gamma=g);
         scores = cross_val_score(clf, X_training, y_training, cv=10, scoring='accuracy');
@@ -240,20 +244,21 @@ def build_SVM_classifier(X_training, y_training):
             best_g = g
      
         g_scores.append(round(scores.mean(), 5));
-     
-    plt.semilogx(g_range, g_scores, label="Training score",
-             color="darkorange", lw=2)
+    
+    #plot
+    plt.semilogx(g_range, g_scores, label="Training score",color="darkorange", lw=2)
     plt.xlabel("Value of gamma for SVM")
     plt.ylabel("Cross-validated SVM")
     plt.show();
     
-    # best_k is the hyperparameter
+    # best_g (gamma) is the hyperparameter
     print("Best g value = ", best_g);
     
+    #take our best parameter and input it in our classifier
     svm = SVC(gamma=best_g);
-    
     svm.fit(X_training, y_training)
     
+    #return the mean of 10 cross validated scores to find cross validated accuracy
     score = cross_val_score(svm, X_training, y_training, cv=10, scoring='accuracy').mean();
      
     print("Cross-validated score with best g: ", score);
@@ -262,21 +267,17 @@ def build_SVM_classifier(X_training, y_training):
     
     return svm;
     
-    
-
-    # raise NotImplementedError()
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 if __name__ == "__main__":
     pass
-    # call your functions here
+
     # Load the dataset and split
     x_training, y_training, x_test, y_test = prepare_dataset("medical_records(1).data");
     
+    print("===============================================================");
     # build Naive Bayes classifier
     print("NAIVE BAYES");
-    print("===============================================================");
     clf = build_NB_classifier(x_training, y_training);
     x_test_NB = x_test.astype(np.float);
     x_training_NB = x_training.astype(np.float);
@@ -325,7 +326,7 @@ if __name__ == "__main__":
     testing_error = 1 - accuracy;
     print("Testing Error (SVM)",testing_error);
     print("Testing accuracy (SVM)", accuracy);
-    
+    print("===============================================================");
     
 ##################################### REPORT SHIT ############################
     
